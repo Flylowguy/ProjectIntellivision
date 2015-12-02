1fbebb266773f270e99dadd6739e486debd8fcab
wpreachuk <will.preachuk@gmail.com>
2015-12-01 21:22:27 -0600

    Final Draft of assembler plus finished test code for PART 4

--- Assembler/test.s
+++ Assembler/test.s
@@ -1,6 +1,14 @@
-addi  r2,r0,5
-addi  r3,r0,10
-cmp  r2,r3
-addi  r2,r0,15
-addi  r3,r0,15
-cmp  r2,r3
+lw  r2,20(r0)
+lw  r3,21(r0)
+addi  r5,r0,4 
+addi  r6,r0,12 
+LOOP lw  r4,0(r2) 
+sw  r5,0(r3) 
+cmp  r5,r4 
+b eq LEDS
+b  LOOP
+LEDS stw  r6,0(r3)
+lw  r4,0(r2) 
+cmp  r5,r4 
+b eq LEDS
+b  LOOP
